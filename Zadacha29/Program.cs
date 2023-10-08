c1e41ada4bcf6dcd20a4383638d88bbf9a3699c4
TselovalnikovaOlga <olg68502518@yandex.ru>
2023-10-08 09:22:47 +0300

    add Zadacha25 Zadacha27 Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] massiv = new int[8];
+
+System.Console.Write("[");
+
+for (int i = 0; i < 8 - 1; i++)    
+    {
+        massiv[i] = new Random().Next(0,100);
+        Console.Write(massiv[i] + ", ");
+    }
+
+massiv[8-1] = new Random().Next(0,100);
+Console.WriteLine(massiv[8-1] + "]");
+
